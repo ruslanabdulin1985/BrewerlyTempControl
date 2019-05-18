5e5848dadce551dce6f8a8c0927ac6de93f81357
Ruslan <ra222rt@student.lnu.se>
2019-05-18 20:38:35 +0200

    Two fermenters setup. Temp sensor connected

--- src/LowLevel/LowLevel.ino
+++ src/LowLevel/LowLevel.ino
@@ -1,14 +1,19 @@
 #include <TFT.h> // Hardware-specific library
 #include <SPI.h>
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+#define ONE_WIRE_BUS 2
+
 #define CS   10
 #define DC   9
 #define RESET  8  
 
-// pin definition for the Leonardo
-// #define CS   7
-// #define DC   0
-// #define RESET  1 
+
+
+
 
 TFT myScreen = TFT(CS, DC, RESET);
 
@@ -16,14 +21,29 @@ TFT myScreen = TFT(CS, DC, RESET);
 int counter = 0;
 // char array to print time
 char printout[4];
+//char printoutTest[5];
+
 
 double temp;
+String tempString;
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Создаем объект DallasTemperature для работы с сенсорами, передавая ему ссылку на объект для работы с 1-Wire.
+DallasTemperature sensors(&oneWire);
+
+
 
 void setup() {
+
+sensors.begin();
+temp = -99;
+
   // put your setup code here, to run once:
  myScreen.begin();  
   myScreen.setTextSize(1);
-  myScreen.background(0,0,0);  // clear the screen with black
+  myScreen.background(0,0,0);  // clear the screen with black  
   myScreen.stroke(255,255,255);
 
  //  Drawing Left Tank
@@ -54,21 +74,51 @@ void setup() {
   myScreen.stroke(0,191,255);
   myScreen.text("DS18b20 : ",10,120);
 
+  
+  myScreen.text("o",25,60);
+  myScreen.text("o",105,60);
+  myScreen.setTextSize(3);
+  myScreen.text("C",35,60);
+  myScreen.text("C",115,60);
 
-myScreen.text("o",20,60);
-myScreen.setTextSize(3);
-myScreen.text("C",30,60);
-
-  delay(1000);  // pause for dramatic effect
+//  delay(1000);  // pause for dramatic effect
+  // start serial port
+ 
+  Serial.begin(9600);
 }
 
 void loop() {
+ 
+
+  
+  sensors.requestTemperatures(); // Send the command to get temperatures
   
   myScreen.setTextSize(2);
-  temp = 20.4;
-  myScreen.text("20,4",20,30);
+ 
   
+  temp = sensors.getTempCByIndex(0);
   
+  Serial.println(temp);
+  
+  if (!tempString.equals(String(temp,5))){
+      tempString = String(temp,5);
+      tempString.toCharArray(printout,5);
+  
+      myScreen.stroke(0,0,0);
+      myScreen.fill(0,0,0);
+      myScreen.rect(20,30,50,20);
+      myScreen.stroke(0,191,255);
+      myScreen.text(printout,20,30);
+
+
+      myScreen.stroke(0,0,25);
+      myScreen.fill(0,0,25);
+      myScreen.rect(100,30,50,20);
+      myScreen.stroke(0,191,255);
+      myScreen.text(printout,100,30);
+    }
+  
+
   delay(1000);  // pause for dramatic effect
   
   // put your main code here, to run repeatedly:
