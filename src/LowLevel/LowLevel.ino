823a6622183eddfe9b425925fc682638ff20dbb6
Ruslan <ruslanabdulin1985@gmail.com>
2019-06-10 15:54:30 +0200

    Code refactoring

--- src/LowLevel/LowLevel.ino
+++ src/LowLevel/LowLevel.ino
@@ -33,19 +33,14 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Создаем объект DallasTemperature для работы с сенсорами, передавая ему ссылку на объект для работы с 1-Wire.
 DallasTemperature sensors(&oneWire);
 
-
-
-void setup() {
-
-sensors.begin();
-temp = -99;
-
-  // put your setup code here, to run once:
- myScreen.begin();  
+void clearScreen(){
   myScreen.setTextSize(1);
   myScreen.background(0,0,0);  // clear the screen with black  
   myScreen.stroke(255,255,255);
+}
 
+void drawLeftTank(){
+  
  //  Drawing Left Tank
   
   myScreen.line(1, 15, 1, 80);
@@ -57,8 +52,13 @@ temp = -99;
   myScreen.line(70, 3, 8, 3);
   myScreen.line(8, 3, 1, 15);
 
-// Dreawing Right Tank
+  myScreen.text("o",25,60);
+  myScreen.setTextSize(3);
+  myScreen.text("C",35,60);
+}
 
+void drawRightTank(){
+  // Dreawing Right Tank
   myScreen.line(83, 15, 83, 80);
   myScreen.line(83, 80, 93, 115);
   myScreen.line(93, 115, 149, 115);
@@ -67,19 +67,57 @@ temp = -99;
   myScreen.line(158, 15, 150, 3);
   myScreen.line(150, 3, 90, 3);
   myScreen.line(90, 3, 83, 15);
+  
+  myScreen.text("o",105,60);
+  myScreen.setTextSize(3);
+  myScreen.text("C",115,60);
+}
 
+void SetTemperature(String tank, char text){
+  
+  myScreen.setTextSize(2);
+  if (tank == "left"){
+      myScreen.stroke(0,0,0);
+      myScreen.fill(0,0,0);
+      myScreen.rect(20,30,50,20);
+      myScreen.stroke(0,191,255);
+      myScreen.text(text,20,30);
+  }
+  if (tank == "right"){
+      myScreen.stroke(0,0,25);
+      myScreen.fill(0,0,25);
+      myScreen.rect(100,30,50,20);
+      myScreen.stroke(0,191,255);
+      myScreen.text(text,100,30);
+  }
+}
+
+double getTemperature(){
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  double out = sensors.getTempCByIndex(0);
+  return out;
+}
+
+void setup() {
+
+sensors.begin();
+temp = -99;
+
+  // put your setup code here, to run once:
+ myScreen.begin();  
+
+clearScreen();
+
+//drawLeftTank();
+//
+//drawRightTank();
+ 
 // Predefined text
 
   
   myScreen.stroke(0,191,255);
   myScreen.text("DS18b20 : ",10,120);
 
-  
-  myScreen.text("o",25,60);
-  myScreen.text("o",105,60);
-  myScreen.setTextSize(3);
-  myScreen.text("C",35,60);
-  myScreen.text("C",115,60);
 
 //  delay(1000);  // pause for dramatic effect
   // start serial port
@@ -87,36 +125,18 @@ temp = -99;
   Serial.begin(9600);
 }
 
-void loop() {
- 
+void loop() { 
+//  double temp = getTemperature();
+  
+  
+  
+//  if (!tempString.equals(String(temp,5))){
+//      tempString = String(temp,5);
+//      tempString.toCharArray(printout,5);
+////      SetTemperature(myScreen, "left", printout);
+//    }
 
-  
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  
-  myScreen.setTextSize(2);
- 
-  
-  temp = sensors.getTempCByIndex(0);
-  
-  Serial.println(temp);
-  
-  if (!tempString.equals(String(temp,5))){
-      tempString = String(temp,5);
-      tempString.toCharArray(printout,5);
-  
-      myScreen.stroke(0,0,0);
-      myScreen.fill(0,0,0);
-      myScreen.rect(20,30,50,20);
-      myScreen.stroke(0,191,255);
-      myScreen.text(printout,20,30);
-
-
-      myScreen.stroke(0,0,25);
-      myScreen.fill(0,0,25);
-      myScreen.rect(100,30,50,20);
-      myScreen.stroke(0,191,255);
-      myScreen.text(printout,100,30);
-    }
+  Serial.println("Test");
   
 
   delay(1000);  // pause for dramatic effect
