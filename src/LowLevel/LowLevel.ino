7178d7ce475bd3e7b7cbb266e837d45a3bc54ce6
Ruslan <ruslanabdulin1985@gmail.com>
2019-09-26 19:21:49 +0200

    PT100 completely solved

--- src/LowLevel/LowLevel.ino
+++ src/LowLevel/LowLevel.ino
@@ -1,8 +1,17 @@
 #include <TFT.h> // Hardware-specific library
 #include <SPI.h>
-
+#include <Adafruit_MAX31865.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+
+// Use software SPI: CS, DI, DO, CLK
+Adafruit_MAX31865 max = Adafruit_MAX31865(A0, A1, A2, A3 );
+
+// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
+#define RREF      430.0
+// The 'nominal' 0-degrees-C resistance of the sensor
+// 100.0 for PT100, 1000.0 for PT1000
+#define RNOMINAL  100.0
 
 
 #define ONE_WIRE_BUS 2
@@ -251,10 +260,10 @@ float getTemperatureResistance(){
   {
 float voltage = getVoltage(A2);
 //double voltage2 = getVoltage(A5);
-Serial.print("VoltageA5 ");
-Serial.println(voltage);
+//Serial.print("VoltageA5 ");
+//Serial.println(voltage);
 float resistance = getResistance2(voltage, 109.2, 5);
-Serial.println(resistance);
+//Serial.println(resistance);
 //resistance = resistance-1.69;
 
 //resistance = getResistance2();
@@ -299,6 +308,7 @@ void getRelayTemp(){
 
 
 void setup() {
+  
 
 pinMode(RELAY1, OUTPUT);
 pinMode(RELAY2, OUTPUT);
@@ -327,8 +337,53 @@ drawRightTank();
   // start serial port
  
   Serial.begin(9600);
+  
+  Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
+
+  max.begin(MAX31865_4WIRE);  // set to 2WIRE or 4WIRE as
+  
   updtaeRelayTempR();
   updtaeRelayTempL();
+
+  
+}
+
+void TempToSerial(){
+  
+  uint16_t rtd = max.readRTD();
+
+  Serial.print("RTD value: "); Serial.println(rtd);
+  float ratio = rtd;
+  ratio /= 32768;
+  Serial.print("Ratio = "); Serial.println(ratio,8);
+  Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
+  Serial.print("Temperature = "); Serial.println(max.temperature(RNOMINAL, RREF));
+
+  // Check and print any faults
+  uint8_t fault = max.readFault();
+  if (fault) {
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    max.clearFault();
+  }
+  Serial.println();
 }
 
 void loop() { 
@@ -362,6 +417,7 @@ void loop() {
     }
   
 //  SetTemperature("right", "test");
+  TempToSerial();
   delay(1000);  // pause 
   
 }
