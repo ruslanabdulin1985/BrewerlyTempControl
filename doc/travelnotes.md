683ff31bf82c7364e5bd94981e5ba1e25c1bbbb4
Ruslan <ruslanabdulin1985@gmail.com>
2019-11-02 23:34:37 +0100

    Try Out Flask

--- doc/travelnotes.md
+++ doc/travelnotes.md
@@ -3,3 +3,6 @@
 <a href="http://www.youtube.com/watch?feature=player_embedded&v=Z1RJmh_OqeA
 " target="_blank"><img src="http://img.youtube.com/vi/Z1RJmh_OqeA/0.jpg" 
 alt="" width="240" height="180" border="10" /></a>
+
+#Another Flask Tutorial
+[The one I am trying to follow](https://www.youtube.com/watch?v=QnDWIZuWYW0)
--- src/Application/__init__.py
+++ src/Application/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from programs import Program1
-
+import datetime
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def hello():
 
 @app.route("/program1")
 def prgr1():
-    return render_template('Program1.html', periods = Program1.periods)
+    return render_template('Program1.html', periods = Program1.periods, datetime=datetime.datetime)
 
 if __name__ == "__main__":
     app.run(debug=True)
